26a6ac3fec3931bf5e4889288098ff62f42b3ef3
iliciuv <72982503+iliciuv@users.noreply.github.com>
2023-09-26 23:59:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import struct
 import numpy as np
 
 def word_list_to_value(words, kind):
-
     # Convert a list of words (16-bit values) to their respective int32 or float32 representations.
     # Parameters:
     #   - words: List of 16-bit values.
@@ -27,7 +26,6 @@ def word_list_to_value(words, kind):
     ]
 
 async def convert_reading(register_list, datatype="int16"):
-
     # Convert a list of register values based on the specified data type.
     # Parameters:
     #   - register_list: List of register values.
@@ -50,7 +48,6 @@ async def convert_reading(register_list, datatype="int16"):
     return readings
 
 async def read_modbus_data(host, port, address, register_length=1, data_type="int16"):
-
     # Asynchronously read data from Modbus server and display it in Streamlit.
     # Parameters:
     #   - host: Modbus server IP address.
@@ -75,18 +72,21 @@ async def read_multiple_modbus_data(host, port, address, register_length, data_t
     return results
 
 def main():
+    # Main function to render the Streamlit interface and handle user interactions
 
-    # Main function to render the Streamlit interface and handle user interactions.
     st.title("Async Modbus Reader")
     cols1, cols2, cols3 = st.columns([1, 1, 1])
+
     with cols1:
         host = st.text_input("Modbus host IP", "45.95.197.176")
         port = st.text_input("Port", "48418")
         address = st.text_input("Address", "14720")
+
     with cols2:
         register_length = st.text_input("Length", "1")
         data_type = st.selectbox("Data Type", ["int16", "uint16", "int16_alt", "int32", "float"])
         attempts = st.selectbox("nº attempts:", [1, 5, 10])
+
     with cols3:
         if st.button("Submit"):
             results = asyncio.run(read_multiple_modbus_data(host, int(port), int(address), int(register_length), data_type, int(attempts)))
