61f7f2465714b731a2a63e503b6985ca1e9ff243
iliciuv <72982503+iliciuv@users.noreply.github.com>
2023-09-26 23:51:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,11 @@ import numpy as np
 def word_list_to_value(words, kind):
 
     # Convert a list of words (16-bit values) to their respective int32 or float32 representations.
-
     # Parameters:
-    # - words: List of 16-bit values.
-    # - kind: Data type for conversion. Either "int32" or "float32".
-
+    #   - words: List of 16-bit values.
+    #   - kind: Data type for conversion. Either "int32" or "float32".
     # Returns:
-    # - List of converted values.
+    #   - List of converted values.
 
     if kind == "int32":
         k = "i"
@@ -30,18 +28,15 @@ def word_list_to_value(words, kind):
 
 async def convert_reading(register_list, datatype="int16"):
 
-    #Convert a list of register values based on the specified data type.
-
-    #Parameters:
-    #- register_list: List of register values.
-    #- num_registers: Number of registers.
-    #- datatype: Data type for conversion. One of "float", "int32", "int16", "uint16", or "int16_alt".
-
-    #Returns:
-    #- List of converted register values.
+    # Convert a list of register values based on the specified data type.
+    # Parameters:
+    #   - register_list: List of register values.
+    #   - num_registers: Number of registers.
+    #   - datatype: Data type for conversion. One of "float", "int32", "int16", "uint16", or "int16_alt".
+    # Returns:
+    #   - List of converted register values.
 
     readings = []
-    # Determine the conversion method based on datatype
     if datatype == "float":
         readings = word_list_to_value(register_list, "float32")
     elif datatype == "int32":
@@ -56,14 +51,13 @@ async def convert_reading(register_list, datatype="int16"):
 
 async def read_modbus_data(host, port, address, register_length=1, data_type="int16"):
 
-    #Asynchronously read data from Modbus server and display it in Streamlit.
-
-    #Parameters:
-    #- host: Modbus server IP address.
-    #- port: Modbus server port.
-    #- address: Starting address for reading.
-    #- register_length: Number of registers to read.
-    #- data_type: Data type for conversion. One of "float", "int32", "int16", or "uint16".
+    # Asynchronously read data from Modbus server and display it in Streamlit.
+    # Parameters:
+    #   - host: Modbus server IP address.
+    #   - port: Modbus server port.
+    #   - address: Starting address for reading.
+    #   - register_length: Number of registers to read.
+    #   - data_type: Data type for conversion. One of "float", "int32", "int16", or "uint16".
 
     client = modbus_for_url(f"tcp://{host}:{port}")
     try:
@@ -73,10 +67,17 @@ async def read_modbus_data(host, port, address, register_length=1, data_type="in
     except Exception as e:
         st.write("Error: ", e)
 
+async def read_multiple_modbus_data(host, port, address, register_length, data_type, attempts):
+    results = []
+    for _ in range(attempts):
+        result = await read_modbus_data(host, port, address, register_length, data_type)
+        results += result
+    return results
+
 def main():
 
     # Main function to render the Streamlit interface and handle user interactions.
-
+    result_join = []
     st.title("Async Modbus Reader")
     cols1, cols2, cols3 = st.columns([1, 1, 1])
     with cols1:
@@ -84,12 +85,13 @@ def main():
         port = st.text_input("Port", "48418")
         address = st.text_input("Address", "14720")
     with cols2:
-        register_length = st.text_input("Longitud", "1")
+        register_length = st.text_input("Length", "1")
         data_type = st.selectbox("Data Type", ["int16", "uint16", "int16_alt", "int32", "float"])
+        attempts = st.selectbox("nº attempts:", [1, 5, 10])
     with cols3:
         if st.button("Submit"):
-            result = asyncio.run(read_modbus_data(host, int(port), int(address), int(register_length), data_type))
-            st.write("Response: ", result)
+            results = asyncio.run(read_multiple_modbus_data(host, int(port), int(address), int(register_length), data_type, int(attempts)))
+            st.write("Response: ", results)
 
 if __name__ == "__main__":
     main()
