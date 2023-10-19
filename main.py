5933ad92c3fb4d8c736a223e6f88109b7da8e992
il    iciuv <miguel.garcia.duch@gmail.com>
2023-10-19 16:19:57 +0200

    last

--- main.py
+++ main.py
@@ -42,11 +42,9 @@ def convert_reading(register_list, datatype="int16"):
     elif datatype == "int32":
         readings = word_list_to_value(register_list, "int32")
     elif datatype == "int16":
-        readings = [np.int16(v) for v in register_list]
-    elif datatype == "uint16":
-        readings = [np.uint16(v) for v in register_list]
-    elif datatype == "int16_new":
         readings = [int(np.int16(v)) for v in register_list]
+    elif datatype == "uint16":
+        readings = [int(np.uint16(v)) for v in register_list]
     return readings
 
 
@@ -92,7 +90,7 @@ def main():
     with cols2:
         register_length = st.selectbox("Length", [1, 2, 4])
         data_type = st.selectbox(
-            "Data Type", ["int16", "uint16", "int16_new", "int32", "float"]
+            "Data Type", ["int16", "uint16", "int32", "float"]
         )
         attempts = st.selectbox("nº attempts:", [1, 3, 5])
     with cols3:
