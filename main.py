2f96726560374ac6de398bcaa2e0969a9373d235
il    iciuv <miguel.garcia.duch@gmail.com>
2023-10-21 16:52:27 +0200

    last

--- main.py
+++ main.py
@@ -1,36 +1,46 @@
 # ModbusAsyncWeb. Single script app @Miguel Garcia Duch (27/09/2023)
 
-from typing import List, Union
-from async_modbus import modbus_for_url
-import struct
-import asyncio
 import streamlit as st
+from async_modbus import modbus_for_url
+import asyncio
+import struct
+import numpy as np
+from typing import List, Union
 
 
 # Function to convert a list of words based on the specific conversion type 'k'
 def word_list_to_value(words: List[int], k: str) -> List[Union[int, float]]:
-    # Use struct.unpack to convert word pairs to the specified format
+    # Special handling for "uint32_alt"
+    if k == "uint32_alt":
+        processed_words = [
+            int(np.uint16(word1)) + int(np.uint16(word2)) * 2**16
+            for word1, word2 in zip(words[::2], words[1::2])
+        ]
+        return [struct.unpack('!' + 'I', struct.pack('!I', word))[0] for word in processed_words]
+
+    # Standard handling for other types
     return [struct.unpack('!'+  k, struct.pack('!HH', *word_pair))[0] for word_pair in zip(words[::2], words[1::2])]
 
 
 # Function to convert a list of register values based on the specified data type
 def convert_reading(register_list: List[int], datatype: str = "int16") -> List[Union[int, float]]:
+    if datatype == "int16":
+        return [int(np.int16(v)) for v in register_list]
+    elif datatype == "uint16":
+        return [int(np.uint16(v)) for v in register_list]
+
     # Dictionary to map data types to their respective conversion types
     type_map = {
-        "float": 'f',       # Floating-point
-        "int32": 'i',       # 32-bit signed integer
-        "uint32": 'I',      # 32-bit unsigned integer
-        "uint32_alt": 'I',  # Alternative 32-bit unsigned integer
-        "int16": 'h',       # 16-bit signed integer
-        "uint16": 'H'       # 16-bit unsigned integer
+        "float": 'f',
+        "int32": 'i',
+        "uint32": 'I',
+        "uint32_alt": 'uint32_alt'  # Use a unique identifier for the special case
     }
 
     # Check if the given data type exists in the type_map
     if datatype in type_map:
-        # Use word_list_to_value for conversion
         return word_list_to_value(register_list, type_map[datatype])
     else:
-        # Raise an error if the datatype is not supported
         raise ValueError(f"Invalid datatype. Expected one of {list(type_map.keys())}, got {datatype}")
 
 
@@ -52,6 +62,7 @@ async def read_modbus_data(host, port, address, register_length=1, data_type="in
         return converted_result
     except Exception as e:
         st.write("Error: ", e)
+        return []  # Ensure we always return a list
 
 
 # Asynchronously manage concurrent calls
@@ -65,8 +76,8 @@ async def read_multiple_modbus_data(
     return results
 
 
-# Main function to render the Streamlit interface and handle user interactions.
 def main():
+    # Main function to render the Streamlit interface and handle user interactions.
 
     st.title(":satellite_antenna: Asynchronous Modbus Reader :satellite_antenna:")
     cols1, cols2, cols3 = st.columns([1, 1, 1])
